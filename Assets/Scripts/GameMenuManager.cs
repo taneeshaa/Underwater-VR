446056a9c98c2ab437b76cd9262737b371029373
Taneesha Kalra <kalrataneesha@gmail.com>
2023-07-12 13:48:52 +0530

    UI

---
+++ Assets/Scripts/GameMenuManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameMenuManager : MonoBehaviour
+{
+    [SerializeField] private Transform head;
+    [SerializeField] private float spawnDistance = 2;
+    [SerializeField] GameObject menu;
+    [SerializeField] private InputActionProperty showButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (showButton.action.WasPerformedThisFrame())
+        {
+            menu.SetActive(!menu.activeSelf);
+
+            menu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+        }
+
+        menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
+        //menu.transform.forward *= -1;
+    }
+}
